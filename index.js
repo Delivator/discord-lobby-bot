5c6c5c28c74710d5d0ddc20172eaf46506a30be0
Delivator <delivatorgaming@gmail.com>
2018-03-15 03:15:34 +0100

    v1.0.1
    Added:
    - Commands (join/leave)
    
    Bug fixes!

--- index.js
+++ index.js
@@ -4,53 +4,89 @@ const settings = require("./config/settings.json");
 
 log = (msg) => {console.log(`[${new Date().toLocaleString()}] ${msg}`)};
 
-function playSong() {
-  let server = client.guilds.get("122778177754890241"),
-      channel = false;
-  if (server) channel = server.channels.get("423266650293272586")
-  if (channel) {
-    channel.join()
-      .then(connection => {
-        log("Successfully connected to a voice channel!");
-        let members = channel.members.size;
-        let dispatcher = connection.playFile(settings.song);
+function playSong(voiceChannel) {
+  if (voiceChannel.guild.voiceConnection) if (voiceChannel.guild.voiceConnection.dispatcher) voiceChannel.guild.voiceConnection.dispatcher.end();
+  voiceChannel.join()
+    .then(connection => {
+      log(`Joined ${connection.channel.name} (${connection.channel.id})`);
 
-        dispatcher.on("error", e => {
-          console.error(e);
-        });
+      let dispatcher = connection.playFile(settings.song);
 
-        dispatcher.on("end", () => {
-          playSong();
-        });
+      dispatcher.on("error", e => {
+        console.error(e);
+      });
 
-        client.on("voiceStateUpdate", (oldMember, newMember) => {
-          members = channel.members.size;
-          if (members > 1) {
-            if (dispatcher.paused) {
-              dispatcher.resume();
-              client.user.setActivity("▶ Mii Plaza");
-              log("Streaming resumed");
-            }
-          } else {
-            if (!dispatcher.paused) {
-              dispatcher.pause();
-              client.user.setActivity("⏸ Mii Plaza");
-              log("Streaming paused");
-            }
-          }
-        });
-      })
-      .catch(console.log);
-  }
+      dispatcher.on("end", () => {
+        if (voiceChannel.guild.loopSong) playSong(voiceChannel);
+      });
+    })
+    .catch(console.error)
 }
 
 client.on("ready", () => {
   log(`Bot ready! Logged in as ${client.user.tag} (${client.user.id})`);
-  playSong();
+  client.user.setActivity(settings.songTitle);
 });
 
-client.on("error", (e) => {
+client.on("error", e => {
   console.error(e);
+});
+
+client.on("message", message => {
+  let guild = message.guild,
+      channel = message.channel,
+      author = message.author,
+      content = message.content,
+      member = message.member;
+  
+  if (!message.isMemberMentioned(client.user)) return;
+  if (!content.startsWith(`<@${client.user.id}>`)) return;
+  if (content.split(" ").length < 2) return;
+
+  let args = content.split(" ").slice(2),
+      cmd = content.split(" ")[1].toLowerCase();
+
+  switch (cmd) {
+    case "join":
+      if (!member.permissions.has("MOVE_MEMBERS")) return message.reply("Insufficient permission (You need the permission to move members)!");
+      if (!member.voiceChannel) return message.reply("You have to be in a voice channel to use this command!");
+      if (guild.voiceConnection) if (member.voiceChannel === guild.voiceConnection.channel) return;
+      if (guild.voiceConnection) {
+        guild.loopSong = false;
+        guild.voiceConnection.disconnect();
+        guild.loopSong = true;
+        playSong(member.voiceChannel);
+      } else {
+        guild.loopSong = true;
+        playSong(member.voiceChannel);
+      }
+      break;
+    case "leave":
+      if (!member.permissions.has("MOVE_MEMBERS")) return message.reply("Insufficient permission (You need the permission to move members)!");
+      guild.loopSong = false;
+      if (guild.voiceConnection) guild.voiceConnection.disconnect();
+      break;
+  }
+});
+
+client.on("voiceStateUpdate", () => {
+  client.guilds.forEach(guild => {
+    if (!guild.voiceConnection) return;
+    if (!guild.voiceConnection.dispatcher) return;
+    let members = guild.voiceConnection.channel.members.size,
+        dispatcher = guild.voiceConnection.dispatcher;
+    if (members > 1) {
+      if (dispatcher.paused) {
+        dispatcher.resume();
+        log(`Music resumed in ${guild.voiceConnection.channel.name} (${guild.voiceConnection.channel.id})`);
+      }
+    } else {
+      if (!dispatcher.paused) {
+        dispatcher.pause();
+        log(`Music paused in ${guild.voiceConnection.channel.name} (${guild.voiceConnection.channel.id})`);
+      }
+    }
+  });
 });
 
 client.login(settings.token)
